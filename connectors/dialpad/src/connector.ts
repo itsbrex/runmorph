a69cd97f1bee8456a17c2208f2d95bd3057c2c94
Henri <HenriChabrand@users.noreply.github.com>
2025-04-14 11:16:56 +0200

    Adde Dialpa

--- connectors/dialpad/src/connector.ts
+++ connectors/dialpad/src/connector.ts
@@ -3,10 +3,24 @@ import { Connector } from "@runmorph/cdk";
 const connector = new Connector({
   id: "dialpad",
   name: "Dialpad",
+  settings: {
+    env: {
+      name: "Environment",
+      type: "text",
+      required: false,
+      description:
+        "Set to 'sandbox' to use the sandbox environment, or leave blank to use the production environment.",
+    },
+  },
   auth: {
     type: "oauth2::authorizationCode",
-    authorizeUrl: "https://dialpad.com/oauth2/authorize",
-    accessTokenUrl: "https://dialpad.com/oauth2/token",
+    authorizeUrl: async ({ connector }) => {
+      const env = await connector.getSetting("env");
+      return `https://${env === "sandbox" ? "sandbox.dialpad" : "dialpad"}.com/oauth2/authorize`;
+    },
+    accessTokenUrl: async ({ connector }) =>
+      `https://${(await connector.getSetting("env")) === "sandbox" ? "sandbox.dialpad" : "dialpad"}.com/oauth2/token`,
+
     defaultScopes: ["recordings_export", "calls:list", "offline_access"],
   },
   proxy: {
